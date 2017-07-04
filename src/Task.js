2139eba1193ec651da224b732b10d3efc69c61ee
Aviv Shaked <aviv.avraham.shaked@zalando.de>
2017-07-04 10:27:21 +0200

    Add Task class

---
+++ src/Task.js
@@ -0,0 +1,125 @@
+import { requestIdleCallback } from './requestIdleCallback';
+
+class Task {
+    /**
+     * @param {function} taskFn
+     * @param {number} id
+     * @param {{ timeout?: number}} options
+     */
+    static _validateConstructorArguments(taskFn, id, options) {
+        if (typeof taskFn !== 'function') {
+            throw new TypeError('taskFn argument must be a function.');
+        }
+        if (typeof id !== 'number') {
+            throw new TypeError('id argument must be a number.');
+        }
+        if (typeof options !== 'object') {
+            throw new TypeError('options argument must be an object.');
+        }
+        if ('timeout' in options && typeof options.timeout !== 'number') {
+            throw new TypeError('options.timeout must be a number.');
+        }
+    }
+
+    /**
+     *
+     * @param {function} taskFn
+     * @param {number} id
+     * @param {{context?: *, binding?: *, timeout?: number, isRunOnce?: boolean, isImmediate?:
+     *     boolean}?} options
+     */
+    constructor(taskFn, id, options = {}) {
+        Task._validateConstructorArguments(taskFn, id, options);
+        const {
+            context = {},
+            binding = {},
+            timeout = 0,
+            isRunOnce = true,
+            isImmediate = true,
+        } = options;
+        this._taskFn = taskFn;
+        this._id = id;
+        this._context = context;
+        this._binding = binding;
+        this._timeout = timeout;
+        this._isRunOnce = isRunOnce;
+        this._isImmediate = isImmediate;
+        this._fireTaskFn = this._fireTaskFn.bind(this);
+    }
+
+    /**
+     * Checks if a received task function is the same as the instance's task function.
+     * @param {function} taskFn
+     * @returns {boolean}
+     */
+    isTaskFn(taskFn) {
+        return this._taskFn === taskFn;
+    }
+
+    /**
+     * @returns {number}
+     */
+    get timeout() {
+        return this._timeout;
+    }
+
+    /**
+     * @returns {boolean}
+     */
+    get isRunOnce() {
+        return this._isRunOnce;
+    }
+
+    /**
+     * @returns {boolean}
+     */
+    get isImmediate() {
+        return this._isImmediate;
+    }
+
+    /**
+     * @returns {number}
+     */
+    get id() {
+        return this._id;
+    }
+
+    /**
+     * Invokes the task function.
+     * The function is bound to "binding" and "context" is passed as argument.
+     */
+    _fireTaskFn() {
+        if (this._taskFn) {
+            this._taskFn.call(this._binding, this._context);
+            if (this._isRunOnce) {
+                // The owner of the task should make sure the task does not run more then once, but
+                // this is a safeguard for race situations on the owner side. This will make sure
+                // the task will run only once when isRunOnce is true.
+                this._taskFn = null;
+            }
+        }
+    }
+
+    /**
+     * Runs the task by passing it through a requestIdleCallback
+     */
+    runTaskOnIdle() {
+        requestIdleCallback(this._fireTaskFn, {
+            timeout: this._timeout,
+        });
+    }
+
+    /**
+     * Runs the task, either by directly firing the task fn,
+     * or by passing it through a requestIdleCallback.
+     */
+    runTask() {
+        if (this._timeout) {
+            this.runTaskOnIdle();
+        } else {
+            this._fireTaskFn();
+        }
+    }
+}
+
+export default Task;
---
+++ src/Task.spec.js
@@ -0,0 +1,137 @@
+import test from 'ava';
+import proxyquire from 'proxyquire';
+import sinon from 'sinon';
+
+const mockIdleFn = sinon.stub();
+const Task = proxyquire.noCallThru()(
+    './Task',
+    {
+        './requestIdleCallback': { requestIdleCallback: mockIdleFn },
+    },
+).default;
+
+test('constructor should create an instance', (t) => {
+    const someFn = () => {};
+    const context = { some: 'context' };
+    const binding = { some: 'binding' };
+    const someOptions = {
+        context,
+        binding,
+        isRunOnce: false,
+    };
+    const task = new Task(someFn, 3, someOptions);
+    t.true(task instanceof Task);
+    t.is(task._taskFn, someFn);
+    t.is(task._id, 3);
+    t.is(task._context, context);
+    t.is(task._binding, binding);
+});
+
+test('contructor should invoke _validateConstructorArguments', (t) => {
+    const someFn = () => {};
+    const context = { some: 'context' };
+    const binding = { some: 'binding' };
+    const someOptions = {
+        context,
+        binding,
+        isRunOnce: false,
+    };
+    sinon.stub(Task, '_validateConstructorArguments');
+    new Task(someFn, 3, someOptions); // eslint-disable-line
+    t.true(Task._validateConstructorArguments.calledWith(someFn, 3, someOptions));
+    Task._validateConstructorArguments.restore();
+});
+
+test('_validateConstructorArguments should throw if taskFn arg is not a function', (t) => {
+    const fn = () => {
+        Task._validateConstructorArguments('not a function', 0, {});
+    };
+
+    const error = t.throws(fn, TypeError);
+    t.is(error.message, 'taskFn argument must be a function.');
+});
+
+test('_validateConstructorArguments should throw if id arg is not a number', (t) => {
+    const fn = () => {
+        Task._validateConstructorArguments(() => {}, 'not a number', {});
+    };
+
+    const error = t.throws(fn, TypeError);
+    t.is(error.message, 'id argument must be a number.');
+});
+
+test('_validateConstructorArguments should throw if options arg is not a n object', (t) => {
+    const fn = () => {
+        Task._validateConstructorArguments(() => {}, 0, 'not an object');
+    };
+
+    const error = t.throws(fn, TypeError);
+    t.is(error.message, 'options argument must be an object.');
+});
+
+test(
+    '_validateConstructorArguments should throw if timeout property exists in options arg but is not a number',
+    (t) => {
+        const fn = () => {
+            Task._validateConstructorArguments(() => {}, 0, { timeout: 'not a number' });
+        };
+
+        const error = t.throws(fn, TypeError);
+        t.is(error.message, 'options.timeout must be a number.');
+    });
+
+test('_validateConstructorArguments should not throw if all arguments are the right type', (t) => {
+    const fn = () => {
+        Task._validateConstructorArguments(() => {}, 0, { timeout: 500 });
+    };
+
+    t.notThrows(fn);
+});
+
+test('_fireTaskFn should invoke _taskFn with _context as argument and _binding as calling site',
+    (t) => {
+        const taskFn = sinon.spy(); // eslint-disable-line func-names
+        const binding = { some: 'object to bind' };
+        const context = { some: 'object as context' };
+        const task = new Task(taskFn, 0, { context, binding, timeout: 500, isRunOnce: true });
+        task._fireTaskFn();
+        t.true(taskFn.calledWith(context));
+        t.is(taskFn.thisValues[0], binding);
+    });
+
+test('_fireTaskFn should nullify _taskFn if isRunOnce is true',
+    (t) => {
+        const taskFn = sinon.spy(); // eslint-disable-line func-names
+        const binding = { some: 'object to bind' };
+        const context = { some: 'object as context' };
+        const task = new Task(taskFn, 0, { context, binding, timeout: 500, isRunOnce: true });
+        task._fireTaskFn();
+        t.is(task._taskFn, null);
+    });
+
+test('runTask should invoke _fireTaskFn if the task has no timeout', (t) => {
+    const task = new Task(() => {}, 0);
+    sinon.stub(task, '_fireTaskFn');
+    task.runTask();
+    t.true(task._fireTaskFn.called);
+});
+
+test('runTask should not invoke requestIdleCallback if the task has no timeout', (t) => {
+    const task = new Task(() => {}, 0);
+    task.runTask();
+    t.true(mockIdleFn.notCalled);
+});
+
+test('runTask should not invoke _fireTaskFn if the task has no timeout', (t) => {
+    const task = new Task(() => {}, 0, { timeout: 1000 });
+    sinon.stub(task, '_fireTaskFn');
+    task.runTask();
+    t.true(task._fireTaskFn.notCalled);
+});
+
+test('runTask should invoke requestIdleCallback if the task has no timeout', (t) => {
+    const task = new Task(() => {}, 0, { timeout: 1000, isRunOnce: false });
+    task.runTask();
+    t.true(mockIdleFn.calledWith(task._fireTaskFn));
+    t.deepEqual(mockIdleFn.args[0][1], { timeout: 1000 });
+});
